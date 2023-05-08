dba672fa81d2ed65859572c1d50c267b49af52db
akosnandornagy <109482995+akosnandornagy@users.noreply.github.com>
2023-05-08 14:48:50 +0200

    Update login class

--- src/main/java/org/example/pages/JiraLogin.java
+++ src/main/java/org/example/pages/JiraLogin.java
@@ -2,6 +2,7 @@ package org.example.pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -11,6 +12,7 @@ public class JiraLogin {
     By userNameJira = By.xpath("//input[@id='login-form-username']");
     By passwordJira = By.xpath("//input[@id='login-form-password']");
     By loginBtn = By.xpath("//input[@id='login']");
+    By userIcon = By.xpath("//img[@alt='User profile for Auto Tester 52']");
     public JiraLogin(WebDriver driver) {
         this.driver = driver;
         this.wait = new WebDriverWait(driver, 10);
@@ -25,6 +27,9 @@ public class JiraLogin {
 
     public void clickLoginButton() {
         wait.until(ExpectedConditions.elementToBeClickable(loginBtn)).click();
+    }
+    public WebElement getUserIcon() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(userIcon));
     }
     public void login(String username, String password) {
         enterUsername(username);
--- src/test/java/org/example/pagetests/LogInTests.java
+++ src/test/java/org/example/pagetests/LogInTests.java
@@ -6,9 +6,7 @@ import org.example.pages.JiraLogin;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -17,8 +15,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class LogInTests {
     private WebDriver driver;
     private JiraLogin jiraLogin;
-    String username = ConfigReader.getUsername();
-    String password = ConfigReader.getPassword();
+    private String username = ConfigReader.getUsername();
+    private String password = ConfigReader.getPassword();
     @BeforeEach
     public void setUp() {
         ChromeOptions options = new ChromeOptions();
@@ -26,16 +24,15 @@ public class LogInTests {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver(options);
         driver.manage().window().maximize();
-        driver.get("https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa");
+        driver.get("https://jira-auto.codecool.metastage.net/");
         jiraLogin = new JiraLogin(driver);
     }
 
     @Test
     public void loginTest() {
         jiraLogin.login(username, password);
-        
-        WebElement loginIcon = driver.findElement(By.xpath("//*[@id=\"user-options\"]/a"));
-        assertTrue(loginIcon.isDisplayed());
+
+        assertTrue(jiraLogin.getUserIcon().isDisplayed());
     }
 
     @AfterEach
