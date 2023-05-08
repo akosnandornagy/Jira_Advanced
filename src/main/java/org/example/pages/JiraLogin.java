64710831c82cec54cb1792af3889836ed58dc3ba
akosnandornagy <109482995+akosnandornagy@users.noreply.github.com>
2023-05-08 14:03:12 +0200

    Add Login page object
    Add login page object with a simple test case

--- src/main/java/org/example/pages/JiraLogin.java
+++ src/main/java/org/example/pages/JiraLogin.java
@@ -1,4 +1,34 @@
 package org.example.pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class JiraLogin {
+    private WebDriver driver;
+    private WebDriverWait wait;
+    By userNameJira = By.xpath("//input[@id='login-form-username']");
+    By passwordJira = By.xpath("//input[@id='login-form-password']");
+    By loginBtn = By.xpath("//input[@id='login']");
+    public JiraLogin(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, 10);
+    }
+    public void enterUsername(String username) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(userNameJira)).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(passwordJira)).sendKeys(password);
+    }
+
+    public void clickLoginButton() {
+        wait.until(ExpectedConditions.elementToBeClickable(loginBtn)).click();
+    }
+    public void login(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLoginButton();
+    }
 }
--- src/test/java/org/example/pagetests/LogInTests.java
+++ src/test/java/org/example/pagetests/LogInTests.java
@@ -1,4 +1,47 @@
 package org.example.pagetests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.example.config.ConfigReader;
+import org.example.pages.JiraLogin;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class LogInTests {
+    private WebDriver driver;
+    private JiraLogin jiraLogin;
+    String username = ConfigReader.getUsername();
+    String password = ConfigReader.getPassword();
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incognito");
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
+        driver.get("https://jira-auto.codecool.metastage.net/secure/Dashboard.jspa");
+        jiraLogin = new JiraLogin(driver);
+    }
+
+    @Test
+    public void loginTest() {
+        jiraLogin.login(username, password);
+        
+        WebElement loginIcon = driver.findElement(By.xpath("//*[@id=\"user-options\"]/a"));
+        assertTrue(loginIcon.isDisplayed());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
